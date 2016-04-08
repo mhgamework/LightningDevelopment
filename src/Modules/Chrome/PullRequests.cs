80783d9af935165db560bd94294b5d76e1009fba
jasperh <jasper.hilven@xenit.eu>
2016-04-08 11:34:12 +0200

    Add finder-core search commands.

--- src/Modules/Chrome/PullRequests.cs
+++ src/Modules/Chrome/PullRequests.cs
@@ -19,7 +19,15 @@ namespace Modules.Chrome
         {
             Process cmd = new Process();
             cmd.StartInfo.FileName = @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe";
-            cmd.StartInfo.Arguments = "https://bitbucket.org/xenit/fred/pull-requests/";
+            if (arguments.Length == 0 || arguments[0].Equals("fr"))
+                cmd.StartInfo.Arguments = "https://bitbucket.org/xenit/fred/pull-requests/";
+            if (arguments.Length > 0 && arguments[0].Equals("frn"))
+                cmd.StartInfo.Arguments = "https://bitbucket.org/xenit/fred/pull-requests/new/";
+            if (arguments.Length > 0 && arguments[0].Equals("fc"))
+                cmd.StartInfo.Arguments = "https://bitbucket.org/xenit/finder-core/pull-requests/";
+            if (arguments.Length > 0 && arguments[0].Equals("fcn"))
+                cmd.StartInfo.Arguments = "https://bitbucket.org/xenit/finder-core/pull-requests/new/";
+
             cmd.Start();
         }
 
