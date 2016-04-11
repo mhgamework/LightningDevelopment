17515af7c5207f2b7b1ca2135051999ccf03edb6
jasperh <jasper.hilven@xenit.eu>
2016-04-11 10:16:13 +0200

    Open LD and LC shortcuts

---
+++ src/Modules/Core/OpenLDLocation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LightningDevelopment;
+using Modules.Git;
+namespace Modules.Alfresco
+{
+    public class OpenLDLocation : IQuickAction
+    {
+        public string Command
+        {
+            get { return "ldloc"; }
+        }
+
+        public void Execute(string[] arguments)
+        {
+
+            Process.Start(@"C:\Users\Jasperhilven\Desktop\LightningDevelopment\src");
+
+        }
+    }
+}
---
+++ src/Modules/Findercore/OpenFredLocation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LightningDevelopment;
+using Modules.Git;
+namespace Modules.Alfresco
+{
+    public class OpenFinderCoreLocation : IQuickAction
+    {
+            public string Command
+            {
+                get { return "fcloc"; }
+            }
+
+            public void Execute(string[] arguments)
+            {
+
+                Process.Start(@"C:\Users\Jasperhilven\Desktop\finder-core");
+
+            }
+    }
+}
